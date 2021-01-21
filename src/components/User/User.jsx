78fca4fcac6543d2d84549f3e2ed6153bac2c7ce
devpato <pevargas2730@gmail.com>
2021-01-21 02:06:11 -0500

    feat(cleanup): cleanup code

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 const User = ({ login, avatar_url }) => (
-    <div className="flex-col-reverse rounded-lg shadow-md m-8 w-2/6 border-1 border-gray-100">
+    <div className="flex-col-reverse rounded-lg shadow-md border-1 border-gray-100">
         <div className="body flex flex-col items-center space-y-4 text-center p-4">
             <div>
                 <img src={avatar_url} className="h-32 w-32 rounded-full mx-auto ring-2 ring-green-500 ring-opacity-50" alt="avatar" />
--- src/containers/SearchBar/SearchBar.jsx
+++ src/containers/SearchBar/SearchBar.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import Button from '../../components/Button/Button';
-import axios from 'axios';
-import { setUsers } from '../../store/data';
-import store from '../..';
 import { useDispatch } from "react-redux";
+import { fetchUsers } from '../../store/users';
 
 const SearchBar = ({ title, url, path }) => {
 
-    const [results, setResults] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const dispatch = useDispatch();
 
@@ -16,28 +13,13 @@ const SearchBar = ({ title, url, path }) => {
     }
 
     const handleSubmit = event => {
-        event.preventDefault();
-        const res = search('api.github.com', 'search/users', searchTerm);
+        dispatch(fetchUsers(searchTerm));
         setSearchTerm('');
-        setResults(res);
     }
 
-    const search = async (baseUrl, path, searchTerm) => {
-        const url = `https://${baseUrl}/${path}`;
-        try {
-            const response = await axios.get(url, {
-                params: {
-                    q: searchTerm
-                }
-            });
-            dispatch(setUsers(response.data.items));
-        } catch (error) {
-            return error;
-        }
-    }
 
     return (
-        <div className="w-full mx-auto md:w-4/12 md:mt-4">
+        <div className="w-full mx-auto md:w-4/12">
             <form onSubmit={handleSubmit}>
                 <div className="shadow sm:rounded-md sm:overflow-hidden">
                     <div className="px-4 py-4 bg-white space-y-4 sm:p-4">
